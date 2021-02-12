b17a4657530f43d7bbe9101e86d80d7386c89bbe
Swathy <swathypanayoor@gmail.com>
2021-02-12 18:18:41 +0530

    Updates

---
+++ precache-manifest.59baa726d8981ec79cd13c037d7866e2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f32448ebb076392d3e30a7dd8714d22",
+    "url": "/jobs4u/index.html"
+  },
+  {
+    "revision": "71d2e8e156f11fe71be6",
+    "url": "/jobs4u/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "c487fc839f8619fcb938",
+    "url": "/jobs4u/static/js/2.670f26c8.chunk.js"
+  },
+  {
+    "revision": "0aee8186d6749ad5d8cab044b9c4b878",
+    "url": "/jobs4u/static/js/2.670f26c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71d2e8e156f11fe71be6",
+    "url": "/jobs4u/static/js/main.f69aac76.chunk.js"
+  },
+  {
+    "revision": "6edb5504cb472f7250c2",
+    "url": "/jobs4u/static/js/runtime-main.fd76c135.js"
+  },
+  {
+    "revision": "929fc09f8dd72718f84cb8117d1ae2bc",
+    "url": "/jobs4u/static/media/call.929fc09f.jpg"
+  },
+  {
+    "revision": "9d9bd293bfa28ab6e0d70fdb69701626",
+    "url": "/jobs4u/static/media/construction.9d9bd293.jpg"
+  },
+  {
+    "revision": "897d32b96097e3a36d581d7e2b3342a6",
+    "url": "/jobs4u/static/media/cook.897d32b9.jpg"
+  },
+  {
+    "revision": "9f582f89aa1c778036488992017eb080",
+    "url": "/jobs4u/static/media/delivery.9f582f89.jpg"
+  },
+  {
+    "revision": "b1ef23b2ee04a604dc195f69ec3024fc",
+    "url": "/jobs4u/static/media/sw.b1ef23b2.jpg"
+  },
+  {
+    "revision": "c16a447e2d793325a172c7a6bba5206e",
+    "url": "/jobs4u/static/media/ww.c16a447e.jpg"
+  }
+]);
